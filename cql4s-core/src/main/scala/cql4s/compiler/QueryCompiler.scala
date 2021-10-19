653d846bb537168ddfb06395f9863ba4639cf377
Fabio Epifani <epifab@gmail.com>
2021-10-19 17:38:41 +0100

    Group by

--- cql4s-core/src/main/scala/cql4s/compiler/QueryCompiler.scala
+++ cql4s-core/src/main/scala/cql4s/compiler/QueryCompiler.scala
@@ -7,20 +7,30 @@ import scala.Tuple.Concat
 trait QueryCompiler[-Q, Input, Output]:
   def build(query: Q): Query[Input, Output]
 
-object QueryCompiler:
-  given select[Keyspace, TableName, TableColumns, Columns, Output, Where <: LogicalExpr, I1 <: Tuple, I2 <: Tuple, Input] (
+trait QueryFragment[-T, I <: Tuple] extends FragmentCompiler[T, I]
+
+object QueryFragment:
+  given select[Keyspace, TableName, TableColumns, Columns, Output, Where <: LogicalExpr, GroupBy, I1 <: Tuple, I2 <: Tuple, I3 <: Tuple] (
     using
     fields: ListFragment[FieldFragment, Columns, I1],
     where: LogicalExprFragment[Where, I2],
-    encoder: EncoderAdapter[I1 Concat I2, Input],
-    decoder: DecoderAdapter[Columns, Output]
-  ): QueryCompiler[Select[Keyspace, TableName, TableColumns, Columns, Where], Input, Output] with
-    def build(select: Select[Keyspace, TableName, TableColumns, Columns, Where]): Query[Input, Output] =
-      val fragment =
-        fields
-          .build(select.fields, ", ")
-          .wrap("SELECT ", s" FROM ${select.table.keyspace.escaped}.${select.table.name.escaped}") ++
+    groupBy: ListFragment[FieldFragment, GroupBy, I3]
+  ): QueryFragment[Select[Keyspace, TableName, TableColumns, Columns, Where, GroupBy], I1 Concat I2 Concat I3] with
+    def build(select: Select[Keyspace, TableName, TableColumns, Columns, Where, GroupBy]): CompiledFragment[I1 Concat I2 Concat I3] =
+      fields
+        .build(select.fields, ", ")
+        .wrap("SELECT ", s" FROM ${select.table.keyspace.escaped}.${select.table.name.escaped}") ++
         where.build(select.where).prepend("WHERE ") ++
+        groupBy.build(select.groupBy, ", ").prepend("GROUP BY ") ++
         Option.when(select.allowFiltering)("ALLOW FILTERING")
 
-      Query(fragment.cql, encoder, decoder)
+
+object QueryCompiler:
+  given [Fields, S <: Select[_, _, Fields, _, _, _], RawInput <: Tuple, Input, Output] (
+    using
+    fragment: QueryFragment[S, RawInput],
+    encoder: EncoderAdapter[RawInput, Input],
+    decoder: DecoderAdapter[Fields, Output]
+  ): QueryCompiler[S, Input, Output] with
+    def build(select: S): Query[Input, Output] =
+      Query(fragment.build(select).cql, encoder, decoder)
