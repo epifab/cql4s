b1529805a5bb177f0211fe18e13e14d3b21a1a0d
Fabio Epifani <epifab@gmail.com>
2021-10-21 17:48:33 +0100

    Refactored ZIO implementation

--- cql4s-cats/src/main/scala/cql4s/CassandraCatsRuntime.scala
+++ cql4s-cats/src/main/scala/cql4s/CassandraCatsRuntime.scala
@@ -8,25 +8,23 @@ import cql4s.dsl.*
 import cql4s.{CassandraConfig, CassandraRuntime}
 
 class CassandraCatsRuntime[F[_]: Sync](protected val session: CqlSession) extends CassandraRuntime[F, [A] =>> fs2.Stream[F, A]]:
-  private def execute(cql: String, params: List[Any]): F[ResultSet] =
-    val statement = statementBuilder(new SimpleStatementBuilder(cql), params).build()
+  private def execute[T <: Statement[T]](statement: Statement[T]): F[ResultSet] =
     Sync[F].blocking(session.execute(statement))
 
   def execute[Input, Output](query: Query[Input, Output]): Input => fs2.Stream[F, Output] =
     (input: Input) =>
       for {
-        resultSet <- fs2.Stream.eval(execute(query.cql, query.encoder.encode(input)))
+        resultSet <- fs2.Stream.eval(execute(CqlStatement(query)(input)))
         row <- fs2.Stream.eval(Sync[F].blocking(Option(resultSet.one()))).repeat.collectWhile { case Some(row) => row }
       } yield query.decoder.decode(row)
 
   def execute[Input](command: Command[Input]): Input => F[Unit] =
     (input: Input) =>
-      execute(command.cql, command.encoder.encode(input)).void
+      execute(CqlStatement(command)(input)).void
 
   def executeBatch[Input](command: Command[Input], batchType: BatchType): Iterable[Input] => F[Unit] =
     (rows: Iterable[Input]) =>
-      val statement = buildBatchStatement(batchType, command)(rows)
-      Sync[F].blocking(session.execute(statement)).void
+      execute(CqlStatement(batchType, command)(rows)).void
 
 
 object CassandraCatsRuntime:
--- cql4s-zio/src/test/scala/cql4s/IntegrationZioSpec.scala
+++ cql4s-zio/src/test/scala/cql4s/IntegrationZioSpec.scala
@@ -1,5 +1,6 @@
 package cql4s
 
+import com.datastax.oss.driver.api.core.CqlSession
 import com.datastax.oss.driver.api.core.cql.BatchType
 import cql4s.dsl.*
 import cql4s.test.CassandraTestConfig
@@ -8,33 +9,31 @@ import cql4s.test.model.*
 import cql4s.test.queries.*
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
-import zio.ZIO
+import zio.{Has, ZIO, ZLayer}
 import zio.stream.ZSink
 
 import java.util.Currency
 
 class IntegrationZioSpec extends AnyFreeSpec with Matchers:
 
-  val cassandraRuntime: ZIO.Release[Any, Throwable, CassandraZioRuntime] = CassandraZioRuntime(CassandraTestConfig)
+  val cassandraRuntime = CassandraZioRuntime(CassandraTestConfig)
   val ioRuntime = zio.Runtime.default
 
   "Can insert / retrieve data" in {
-    val result: ZIO[Any, Throwable, (Option[Event], Option[Event])] = cassandraRuntime(cassandra =>
-      for {
-        _ <- cassandra.execute(truncateEvents)(())
-        _ <- cassandra.executeBatch(insertEvent, BatchType.LOGGED)(List(event1, event2))
-        _ <- cassandra.execute(findEventById)(event1.id).tap(e => cassandra.execute(updateEventTickets)((
-          Map(Currency.getInstance("USD") -> 32),
-          e.metadata.copy(updatedAt = Some(now)),
-          e.id
-        ))).run(ZSink.drain)
-        updatedEvent <- cassandra.execute(findEventById)(event1.id).run(ZSink.last[Event])
-        _ <- cassandra.execute(deleteEvent)(event1.id)
-        deletedEvent <- cassandra.execute(findEventById)(event1.id).run(ZSink.last[Event])
-      } yield (updatedEvent, deletedEvent)
-    )
+    val program: ZIO[Has[CqlSession], Throwable, (Option[Event], Option[Event])] = for {
+      _ <- CassandraZioRuntime.execute(truncateEvents)(())
+      _ <- CassandraZioRuntime.executeBatch(insertEvent, BatchType.LOGGED)(List(event1, event2))
+      _ <- CassandraZioRuntime.execute(findEventById)(event1.id).tap(e => CassandraZioRuntime.execute(updateEventTickets)((
+        Map(Currency.getInstance("USD") -> 32),
+        e.metadata.copy(updatedAt = Some(now)),
+        e.id
+      ))).run(ZSink.drain)
+      updatedEvent <- CassandraZioRuntime.execute(findEventById)(event1.id).run(ZSink.last[Event])
+      _ <- CassandraZioRuntime.execute(deleteEvent)(event1.id)
+      deletedEvent <- CassandraZioRuntime.execute(findEventById)(event1.id).run(ZSink.last[Event])
+    } yield (updatedEvent, deletedEvent)
 
-    ioRuntime.unsafeRun(result) shouldBe (
+    ioRuntime.unsafeRun(program.provideLayer(cassandraRuntime)) shouldBe (
       Some(event1.copy(
         tickets = Map(Currency.getInstance("USD") -> 32),
         metadata = event1.metadata.copy(updatedAt = Some(now))
